998dc9275fe1356f17581fb4bfa9239722ceb49f
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-04-04 19:03:16 +0100

    [feature] first push

---
+++ js/main.js
@@ -0,0 +1,97 @@
+$(document).ready(() => {
+    $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    console.log(searchText)
+getMovies(searchText);
+e.preventDefault();
+});
+});
+
+$('#animate').click(function() {
+        $('html, body').animate({ scrollTop: 500 }, 'slow');
+    });
+
+
+function getMovies(searchText) {
+    console.log(searchText);
+    axios.get('https://www.omdbapi.com?s='+ searchText+'&apikey=15edf019')
+        .then((response) => {
+        console.log(response);
+        let movies =  response.data.Search;
+        let output = '';
+        $.each(movies, (index, movie) => {
+          output += `
+            <div class="col-md-4 wow animated-">
+            <div class="card">
+            <img src="${movie.Poster}" class="card-img-top img-fluid">
+            <div class="card-body text-center">
+            <h4 class="card-title text-center h4-responsive textcount" >${movie.Title}</h4>
+            <a onclick="movieSelected('${movie.imdbID}')" class="btn original elegant-color btn-md" href="#">Movie Details</a>
+            </div>
+            </div> 
+            </div>       
+          `;
+        });
+
+        $('#movies').html(output);
+
+})
+.catch((err) => {
+        console.log(err)
+})
+};
+
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+
+function getMovie() {
+    let movieId = sessionStorage.getItem('movieId');
+
+    axios.get('https://www.omdbapi.com?i='+ movieId+'&apikey=15edf019')
+        .then((response) => {
+            console.log(response);
+            let movie = response.data;
+
+            let output = `
+               <div class="row">
+               <div class="col-md-4">
+               <div class="card">
+               <img src="${movie.Poster}" class="card-img-top img-fluid">
+                </div>
+                </div>
+                <div class="col-md-8">
+                <h2>${movie.Title}</h2>
+                <ul class="list-group">
+                <li class="list-group-item"><strong>Genre: </strong> ${movie.Genre}</li>
+                <li class="list-group-item"><strong>Rated: </strong> ${movie.Rated}</li>
+                <li class="list-group-item"><strong>IMDB Rating: </strong> ${movie.imdbRating}</li>
+                <li class="list-group-item"><strong>Director: </strong> ${movie.Director}</li>
+                <li class="list-group-item"><strong>Writer: </strong> ${movie.Writer}</li>
+                <li class="list-group-item"><strong>Actors: </strong> ${movie.Actors}</li>
+                <li class="list-group-item"><strong>Awards: </strong> ${movie.Awards}</li>
+                <li class="list-group-item"><strong>Box Office: </strong> ${movie.BoxOffice}</li>
+</ul>
+</div>
+               
+</div>
+
+            <div class="row">
+            <div class="card-body">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="index.html" class="btn btn-light">Back To Search</a>
+</div>
+</div>
+        `;
+            $('#movie').html(output);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
